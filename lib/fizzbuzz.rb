a8d0bdedd34ab432aab160a48aaae74b2d231214
Angela Wolff <warriorwomenblog@gmail.com>
2018-06-25 18:28:20 +0100

    Populated fizzbuzz_spec with more robust tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,17 @@
+# "Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz”
+# for the multiples of five print “Buzz”.
+# For multiples of both three and five print “FizzBuzz”."
+
+
 def fizzbuzz(number)
-  'fizz'
+  if number % 5 == 0 && number % 3 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
+    'fizz'
+  elsif number % 5 == 0
+    'buzz'
+  else
+    number
+  end
 end
